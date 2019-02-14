7bcd6d740091ade13b925f483dc7b7ba93c9a6d8
Soroush Khosravi <sorush.kh68@gmail.com>
2019-02-14 13:27:30 +0000

    Polymorphism done.

--- lib/scrambleDiary.rb
+++ lib/scrambleDiary.rb
@@ -4,14 +4,7 @@ class ScrambledDiary
   end
 
   def to_do(a)
-    a.do()
-  end
-  def scramble_by_reversing
-    @contents = @contents.reverse
-  end
-
-  def unscramble_by_reversing
-    @contents = @contents.reverse
+    @contents = a.do()
   end
 end
 
@@ -54,8 +47,13 @@ class Read
     @contents
   end
 end
-puts ScrambledDiary.new("abc").to_do(Scramble_by_advancing_chars.new("abc",10))
 
-puts ScrambledDiary.new("abc").to_do(Unscramble_by_advancing_chars.new("klm",10))
-
-puts ScrambledDiary.new("abc").to_do(Read.new("abc"))
+class Scramble_by_reversing
+  def initialize(contents , step = nil)
+    @contents = contents
+    @step = step
+  end
+  def do
+    @contents.reverse()
+  end
+end
